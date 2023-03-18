a4d166d94aade3cc2303c0ef977b0e0ed74493c0
Tbence98 <tlrbence@gmail.com>
2023-03-18 23:43:06 +0100

    prepare characters-context for clash simulation

--- src/store/characters-context.jsx
+++ src/store/characters-context.jsx
@@ -3,11 +3,32 @@ import React, { useState } from "react";
 const CharactersContext = React.createContext({
     characters: null,
     addCharacters: (characters) => {},
+    addClashingCharacter: (character) => {},
+    removeClashingCharacter: (characterId) => {},
     clear: () => {},
 });
 
 export const CharactersContextProvider = (props) => {
     const [characters, setCharacters] = useState(null);
+    const [clashingCharacters, setClashingCharacters] = useState([]);
+
+    function addClashingCharacter(character) {
+        if (clashingCharacters.length >= 2) {
+            throw new RangeError("You can only have 2 clashingCharacters");
+        }
+
+        const newClashingCharacters = [...clashingCharacters];
+        newClashingCharacters.push(character);
+        setClashingCharacters(newClashingCharacters);
+    }
+
+    function removeClashingCharacter(characterId) {
+        const filteredClashingCharacters = clashingCharacters.filter(
+            (character) => character.id !== characterId
+        );
+
+        setClashingCharacters(filteredClashingCharacters);
+    }
 
     function addCharacters(characters) {
         setCharacters(characters);
@@ -15,11 +36,15 @@ export const CharactersContextProvider = (props) => {
 
     function clear() {
         setCharacters(null);
+        setClashingCharacters([]);
     }
 
     const value = {
         characters,
+        clashingCharacters,
         addCharacters,
+        addClashingCharacter,
+        removeClashingCharacter,
         clear,
     };
 
