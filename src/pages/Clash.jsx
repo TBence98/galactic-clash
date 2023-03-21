9bebcc976f7d33d93c69c515e6be6442dc55d06a
Tbence98 <tlrbence@gmail.com>
2023-03-21 14:10:59 +0100

    add animation for the clash winner

--- src/pages/Clash.jsx
+++ src/pages/Clash.jsx
@@ -55,88 +55,102 @@ const Clash = () => {
     const darkCharacterName = darkCharacter.name.split("<br>").join(" ");
     const lightCharacterName = lightCharacter.name.split("<br>").join(" ");
 
-    if (darkCharacterHp <= 0) {
-        return (
-            <>
-                <p className={classes.main_title}>a csata nyertese</p>
-                <div className={classes.winner_container}>
-                    <div className={classes.winner_img_container}>
-                        {lightCharacterImg}
-                    </div>
-                    <span className={classes.character_name}>
-                        {lightCharacterName}
-                    </span>
-                    <Link
-                        to="/select-character"
-                        className={classes.back_to_board_link}
-                    >
-                        Vissza a fedélzetra
-                    </Link>
-                </div>
-            </>
-        );
-    }
-
-    if (lightCharactedHp <= 0) {
-        return (
-            <>
-                <p className={classes.main_title}>a csata nyertese</p>
-                <div className={classes.winner_container}>
-                    <div className={classes.winner_img_container}>
-                        {darkCharacterImg}
-                    </div>
-                    <span className={classes.character_name}>
-                        {darkCharacterName}
-                    </span>
-                    <Link
-                        to="/select-character"
-                        className={classes.back_to_board_link}
-                    >
-                        Vissza a fedélzetra
-                    </Link>
-                </div>
-            </>
-        );
-    }
+    const darkSideContainerClasses = `${classes.clash_container__dark_side} ${
+        darkCharacterHp <= 0
+            ? classes.hide
+            : lightCharactedHp <= 0
+            ? classes.dark_animation
+            : ""
+    }`;
+    const lightSideContainerClasses = `${classes.clash_container__light_side} ${
+        lightCharactedHp <= 0
+            ? classes.hide
+            : darkCharacterHp <= 0
+            ? classes.light_animation
+            : ""
+    }`;
 
     return (
         <>
             <p className={classes.main_title}>a tudás legyen veled!</p>
             <div className={classes.clash_container}>
-                <div className={classes.clash_container__dark_side}>
+                <div className={darkSideContainerClasses}>
                     <span className={classes.side_text}>Sötét oldal</span>
                     <div className={classes.character_img_container}>
                         {darkCharacterImg}
                     </div>
-                    <span className={classes.character_name}>
-                        {darkCharacterName}
-                    </span>
-                    <HealthBar
-                        side="DARK"
-                        progress={darkCharacterHp.toString()}
-                    />
-                    <span className={classes.healt_percentage}>
-                        {darkCharacterHp}%
-                    </span>
+                    <div>
+                        <span
+                            className={`${classes.character_name} ${
+                                lightCharactedHp <= 0
+                                    ? classes["character_name--center"]
+                                    : ""
+                            }`}
+                        >
+                            {darkCharacterName}
+                        </span>
+                    </div>
+                    {lightCharactedHp <= 0 ? (
+                        <Link
+                            to="/select-character"
+                            className={classes.back_to_board_link}
+                        >
+                            Vissza a fedélzetra
+                        </Link>
+                    ) : (
+                        <>
+                            <HealthBar
+                                side="DARK"
+                                progress={darkCharacterHp.toString()}
+                            />
+
+                            <span className={classes.healt_percentage}>
+                                {darkCharacterHp}%
+                            </span>
+                        </>
+                    )}
                 </div>
-                <div className={classes.versus_container}>
+                <div
+                    className={`${classes.versus_container} ${
+                        lightCharactedHp <= 0 || darkCharacterHp <= 0
+                            ? classes.hide
+                            : ""
+                    }`}
+                >
                     <span>VS</span>
                 </div>
-                <div className={classes.clash_container__light_side}>
+                <div className={lightSideContainerClasses}>
                     <span className={classes.side_text}>Világos oldal</span>
                     <div className={classes.character_img_container}>
                         {lightCharacterImg}
                     </div>
-                    <span className={classes.character_name}>
+                    <span
+                        className={`${classes.character_name} ${
+                            darkCharacterHp <= 0
+                                ? classes["character_name--center"]
+                                : ""
+                        }`}
+                    >
                         {lightCharacterName}
                     </span>
-                    <HealthBar
-                        side="LIGHT"
-                        progress={lightCharactedHp.toString()}
-                    />
-                    <span className={classes.healt_percentage}>
-                        {lightCharactedHp}%
-                    </span>
+                    {darkCharacterHp <= 0 ? (
+                        <Link
+                            to="/select-character"
+                            className={classes.back_to_board_link}
+                        >
+                            Vissza a fedélzetra
+                        </Link>
+                    ) : (
+                        <>
+                            <HealthBar
+                                side="LIGHT"
+                                progress={lightCharactedHp.toString()}
+                            />
+                            <span className={classes.healt_percentage}>
+                                {lightCharactedHp}%
+                            </span>
+                        </>
+                    )}
                 </div>
             </div>
         </>
